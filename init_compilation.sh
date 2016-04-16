50b6c5605e8f9f74f4d0bc31f3ea8350b8136b16
Full Bright <full3right@gmail.com>
2016-04-16 09:07:17 +0200

    Correct init script

--- init_compilation.sh
+++ init_compilation.sh
@@ -2,10 +2,16 @@
 
 sudo apt-get install -y linux-headers-generic build-essential git
 
-cd src
+cd $(dirname $0)/src
 
 make
 sudo make install
 sudo mkdir -p /etc/Wireless/RT2870STA/
 sudo cp RT2870STA.dat /etc/Wireless/RT2870STA/
+#sudo cp RT2870STA.dat /lib/firmware/
 sudo modprobe mt7601Usta
+
+#echo "Adding the newly compiled module to /etc/modules"
+#if grep -q "mt7601Usta" /etc/modules; then
+#    echo "mt7601Usta" >> /etc/modules
+#fi
